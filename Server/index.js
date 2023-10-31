be32c1963396f08cc222b86ff0c392fb5c418cef
asenjialvin <asenjialvin@gmail.com>
2023-10-31 15:35:34 +0300

    updated db.js

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from 'cors'
 import { adminRouter } from "./Routes/AdminRoute.js";
-import { EmployeeRouter } from "./Routes/EmployeeRoute.js";
+import { AttacheeRouter } from "./Routes/AttacheeRoute.js";
 import Jwt from "jsonwebtoken";
 import cookieParser from "cookie-parser";
 
@@ -14,7 +14,7 @@ app.use(cors({
 app.use(express.json())
 app.use(cookieParser())
 app.use('/auth', adminRouter)
-app.use('/employee', EmployeeRouter)
+app.use('/attachee', AttacheeRouter)
 app.use(express.static('Public'))
 
 const verifyUser = (req, res, next) => {
